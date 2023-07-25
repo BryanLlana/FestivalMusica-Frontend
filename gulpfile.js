2398e779f041b245ecdade770b617fe3d2254708
Bryan Joseph <bryanllana14@gmail.com>
2023-07-25 16:16:14 -0500

    Header y video terminados

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
 import gulp from 'gulp'
 import gulpSass from 'gulp-sass'
 import * as sassPkg from 'sass'
+import gulpPlumber from 'gulp-plumber'
 
 const { src, dest, watch } = gulp
 const sass = gulpSass(sassPkg)
 
 const scss = (done) => {
   src('src/scss/app.scss')
+    .pipe(gulpPlumber())
     .pipe(sass())
     .pipe(dest('dist/css'))
   done()
 }
 
 const dev = () => {
-  watch('src/scss/app.scss', scss)
+  watch('src/scss/**/*.scss', scss)
 }
 
 export {
