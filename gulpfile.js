6de0b4024e4438e942e262e68888207501bed2f3
Bryan Joseph <bryanllana14@gmail.com>
2023-07-26 14:19:54 -0500

    Convertir imagenes a webp, avif y minificarlos configurado

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,15 @@ import gulpSass from 'gulp-sass'
 import * as sassPkg from 'sass'
 import gulpPlumber from 'gulp-plumber'
 
-const { src, dest, watch } = gulp
+import gulpImageMin from 'gulp-imagemin'
+import gulpCache from 'gulp-cache'
+import gulpWebp from 'gulp-webp'
+import gulpAvif from 'gulp-avif'
+
+const { src, dest, watch, parallel } = gulp
 const sass = gulpSass(sassPkg)
 
-const scss = (done) => {
+const scss = done => {
   src('src/scss/app.scss')
     .pipe(gulpPlumber())
     .pipe(sass())
@@ -14,12 +19,45 @@ const scss = (done) => {
   done()
 }
 
+const imagenes = done => {
+  src('src/img/**/*.{png,jpg}')
+    .pipe(gulpCache(gulpImageMin({
+      optimizationLevel: 3
+    })))
+    .pipe(dest('dist/img'))
+  done()
+}
+
+const webp = done => {
+  src('src/img/**/*.{png,jpg}')
+    .pipe(gulpWebp({
+      quality: 50
+    }))
+    .pipe(dest('dist/img'))
+  done()
+}
+
+const avif = done => {
+  src('src/img/**/*.{png,jpg}')
+    .pipe(gulpAvif({
+      quality: 50
+    }))
+    .pipe(dest('dist/img'))
+  done()
+}
+
 const dev = () => {
   watch('src/scss/**/*.scss', scss)
 }
 
+const devParallel = parallel(imagenes, webp, avif, dev)
+
 export {
   scss,
-  dev
+  webp,
+  imagenes,
+  avif,
+  dev,
+  devParallel
 }
 
